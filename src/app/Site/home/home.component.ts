c163817a2d5b06e6830ffc34e2362066a99f57b5
nagydavi <nagyfalusidavid@gmail.com>
2024-04-23 00:05:31 +0200

    Footer home service

--- src/app/Site/home/home.component.ts
+++ src/app/Site/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
+import { FooterComponent } from "../footer/footer.component";
+import { MatCard } from '@angular/material/card';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [MenuComponent]
+    imports: [
+        MenuComponent,
+        FooterComponent,
+        MatCard]
 })
 export class HomeComponent {
 
